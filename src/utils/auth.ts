b9435ef1bb064934115ec408593b7b1f5ae8b3cd
Seyed-Cj <seyedxcj@gmail.com>
2025-11-29 22:15:42 +0330

    auth utils

---
+++ src/utils/auth.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SALT_ROUND = 10;
+
+export const hashPassword = (password: string) => 
+  bcrypt.hash(password, SALT_ROUND);
+
+export const comparePassword = (password: string, hash: string) =>
+  bcrypt.compare(password, hash);
+
+export const signToken = (payload: object) =>
+  jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: "7d" });
