1eceece71c5ba7db6ac13d6c357bf4699f47a1ff
Seyed-Cj <seyedxcj@gmail.com>
2025-12-05 20:00:52 +0330

    update role middleware

--- src/middleware/role.middleware.ts
+++ src/middleware/role.middleware.ts
@@ -1,7 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.user?.role !== 'admin') return res.status(403).json({ message: 'Access denied .' });
+  if (!req.user) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Access denied.' });
+  }
 
   next();
 };
